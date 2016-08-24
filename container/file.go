f5a2fc80fd8730f3ba09c5c1bdbb8fbc4e26da12
Zack Patrick <zack.patrick@us.imshealth.com>
2016-08-24 13:38:38 -0700

    add human readable mode for file container

--- container/file.go
+++ container/file.go
@@ -9,18 +9,35 @@ import (
 )
 
 type FileContainer struct {
-	path  string
-	mutex *sync.Mutex
+	path      string
+	mutex     *sync.Mutex
+	getTables func(path string, tableID string) (map[string]map[string][]byte, error)
+	setTables func(path string, tables map[string]map[string][]byte) error
 }
 
-func NewFileContainer(path string, mutex *sync.Mutex) *FileContainer {
+func NewByteFileContainer(path string, mutex *sync.Mutex) *FileContainer {
 	if mutex == nil {
 		mutex = &sync.Mutex{}
 	}
 
 	return &FileContainer{
-		path:  path,
-		mutex: mutex,
+		path:      path,
+		mutex:     mutex,
+		getTables: getByteTables,
+		setTables: setByteTables,
+	}
+}
+
+func NewStringFileContainer(path string, mutex *sync.Mutex) *FileContainer {
+	if mutex == nil {
+		mutex = &sync.Mutex{}
+	}
+
+	return &FileContainer{
+		path:      path,
+		mutex:     mutex,
+		getTables: getStringTables,
+		setTables: setStringTables,
 	}
 }
 
@@ -28,16 +45,19 @@ func (this *FileContainer) Init(tableID string) error {
 	this.mutex.Lock()
 	defer this.mutex.Unlock()
 
-	if _, err := os.Stat(this.path); err != nil {
-		ioutil.WriteFile(this.path, []byte("{}"), os.FileMode(0777))
+	file, err := os.Stat(this.path)
+	if err != nil || file.Size() == 0 {
+		if err := ioutil.WriteFile(this.path, []byte("{}"), os.FileMode(0777)); err != nil {
+			return err
+		}
 	}
 
-	tables, err := this.getTables(tableID)
+	tables, err := this.getTables(this.path, tableID)
 	if err != nil {
 		return err
 	}
 
-	return this.setTables(tables)
+	return this.setTables(this.path, tables)
 }
 
 func (this *FileContainer) Select(tableID, query string) (map[string][]byte, error) {
@@ -48,7 +68,7 @@ func (this *FileContainer) SelectAll(tableID string) (map[string][]byte, error) 
 	this.mutex.Lock()
 	defer this.mutex.Unlock()
 
-	tables, err := this.getTables(tableID)
+	tables, err := this.getTables(this.path, tableID)
 	if err != nil {
 		return nil, err
 	}
@@ -60,7 +80,7 @@ func (this *FileContainer) Insert(tableID, key string, entry []byte) error {
 	this.mutex.Lock()
 	defer this.mutex.Unlock()
 
-	tables, err := this.getTables(tableID)
+	tables, err := this.getTables(this.path, tableID)
 	if err != nil {
 		return err
 	}
@@ -71,14 +91,14 @@ func (this *FileContainer) Insert(tableID, key string, entry []byte) error {
 
 	tables[tableID][key] = entry
 
-	return this.setTables(tables)
+	return this.setTables(this.path, tables)
 }
 
 func (this *FileContainer) Delete(tableID, key string) (bool, error) {
 	this.mutex.Lock()
 	defer this.mutex.Unlock()
 
-	tables, err := this.getTables(tableID)
+	tables, err := this.getTables(this.path, tableID)
 	if err != nil {
 		return false, err
 	}
@@ -88,11 +108,11 @@ func (this *FileContainer) Delete(tableID, key string) (bool, error) {
 	}
 
 	delete(tables[tableID], key)
-	return true, this.setTables(tables)
+	return true, this.setTables(this.path, tables)
 }
 
-func (this *FileContainer) getTables(tableID string) (map[string]map[string][]byte, error) {
-	bytes, err := ioutil.ReadFile(this.path)
+func getByteTables(path, tableID string) (map[string]map[string][]byte, error) {
+	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
@@ -113,11 +133,74 @@ func (this *FileContainer) getTables(tableID string) (map[string]map[string][]by
 	return tables, nil
 }
 
-func (this *FileContainer) setTables(tables map[string]map[string][]byte) error {
-	bytes, err := json.MarshalIndent(tables, "", "    ")
+func setByteTables(path string, tables map[string]map[string][]byte) error {
+	bytes, err := json.Marshal(tables)
 	if err != nil {
 		return nil
 	}
 
-	return ioutil.WriteFile(this.path, bytes, os.FileMode(0777))
+	return ioutil.WriteFile(path, bytes, os.FileMode(0777))
+}
+
+func getStringTables(path, tableID string) (map[string]map[string][]byte, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var decodedTables map[string]map[string]interface{}
+	if err := json.Unmarshal(bytes, &decodedTables); err != nil {
+		return nil, err
+	}
+
+	if decodedTables == nil {
+		decodedTables = map[string]map[string]interface{}{}
+	}
+
+	encodedTables := map[string]map[string][]byte{}
+	for tableID, tableKeys := range decodedTables {
+		for key, value := range tableKeys {
+			if _, ok := encodedTables[tableID]; !ok {
+				encodedTables[tableID] = map[string][]byte{}
+			}
+
+			bytes, err := json.Marshal(value)
+			if err != nil {
+				return nil, err
+			}
+
+			encodedTables[tableID][key] = bytes
+		}
+	}
+
+	if _, ok := encodedTables[tableID]; !ok {
+		encodedTables[tableID] = map[string][]byte{}
+	}
+
+	return encodedTables, nil
+}
+
+func setStringTables(path string, encodedTables map[string]map[string][]byte) error {
+	decodedTables := map[string]map[string]interface{}{}
+	for tableID, tableKeys := range encodedTables {
+		for key, value := range tableKeys {
+			if _, ok := decodedTables[tableID]; !ok {
+				decodedTables[tableID] = map[string]interface{}{}
+			}
+
+			var item interface{}
+			if err := json.Unmarshal(value, &item); err != nil {
+				return err
+			}
+
+			decodedTables[tableID][key] = item
+		}
+	}
+
+	bytes, err := json.MarshalIndent(decodedTables, "", "    ")
+	if err != nil {
+		return nil
+	}
+
+	return ioutil.WriteFile(path, bytes, os.FileMode(0777))
 }
